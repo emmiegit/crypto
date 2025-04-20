e8ac9a7fa07770b6309d03d999c8cf76a4755f0f
Emmie Maeda <emmie.maeda@gmail.com>
2025-04-19 23:15:03 -0400

    Add CipherMode enum.

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,9 @@
 use generic_array::GenericArray;
 
 pub type ByteArray<N> = GenericArray<u8, N>;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum CipherMode {
+    Encrypt,
+    Decrypt,
+}
