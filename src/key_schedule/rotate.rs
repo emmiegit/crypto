a5deb73b114b4c8dd82cb2cc16356c76beeef20b
Emmie Maeda <emmie.maeda@gmail.com>
2025-04-20 02:13:18 -0400

    Move slice_to_u64 to utils.rs

--- src/key_schedule/rotate.rs
+++ src/key_schedule/rotate.rs
@@ -1,4 +1,5 @@
 use super::KeySchedule;
+use crate::utils::slice_to_u64;
 
 pub type RoundKey = [u8; 16];
 
@@ -29,12 +30,6 @@ impl KeySchedule<RoundKey> for SimpleRotateKeySchedule {
     }
 }
 
-fn slice_to_u64(bytes: &[u8]) -> u64 {
-    let mut array = [0; 8];
-    array.as_mut_slice().copy_from_slice(&bytes);
-    u64::from_be_bytes(array)
-}
-
 #[test]
 fn simple() {
     let mut key_sched = SimpleRotateKeySchedule::new([
@@ -57,21 +52,4 @@ fn simple() {
             0xff, 0xff,
         ]
     );
-}
-
-#[test]
-fn test_bytes() {
-    macro_rules! check {
-        ($num:expr, $bytes:expr $(,)?) => {
-            assert_eq!(
-                slice_to_u64(&$bytes),
-                $num,
-                "Actual extracted value does not match expected",
-            );
-        };
-    }
-
-    check!(0xdeadbeef00ff11ee, [0xde, 0xad, 0xbe, 0xef, 0x00, 0xff, 0x11, 0xee]);
-    check!(0x0123456789abcdef, [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]);
-    check!(0x00000000ffffffff, [0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff]);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -38,3 +38,35 @@ fn test_xor() {
         [0xed, 0xcb, 0xa9, 0x87],
     );
 }
+
+pub fn slice_to_u64(bytes: &[u8]) -> u64 {
+    let mut array = [0; 8];
+    array.as_mut_slice().copy_from_slice(&bytes);
+    u64::from_be_bytes(array)
+}
+
+#[test]
+fn test_slice_to_u64() {
+    macro_rules! check {
+        ($num:expr, $bytes:expr $(,)?) => {
+            assert_eq!(
+                slice_to_u64(&$bytes),
+                $num,
+                "Actual extracted value does not match expected",
+            );
+        };
+    }
+
+    check!(
+        0xdeadbeef00ff11ee,
+        [0xde, 0xad, 0xbe, 0xef, 0x00, 0xff, 0x11, 0xee]
+    );
+    check!(
+        0x0123456789abcdef,
+        [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef]
+    );
+    check!(
+        0x00000000ffffffff,
+        [0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff]
+    );
+}
